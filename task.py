d6b68c1613a72e801da92c66e12b5cde3b67f600
moreyb <138537564+moreyb@users.noreply.github.com>
2024-07-04 20:32:22 -0300

    task.py

---
+++ task.py
@@ -0,0 +1,11 @@
+def actualizar_precios():
+    # Lógica para actualizar precios
+    print("Actualizando precios de productos...")
+
+def enviar_correos_recordatorio():
+    # Lógica para enviar correos electrónicos
+    print("Enviando correos de recordatorio a clientes...")
+
+def generar_informe_ventas():
+    # Lógica para generar informe de ventas
+    print("Generando informe de ventas diario...")
